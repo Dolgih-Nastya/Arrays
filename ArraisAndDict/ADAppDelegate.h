3a24668a807daef0f3a82e65f86b35c86883aebc
Анастасия Долгих <anastasiadolgih@Anastasias-Mac.local>
2014-04-17 04:01:49 -0700

    Initial Commit

---
+++ ArraisAndDict/ADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADAppDelegate.h
+//  ArraisAndDict
+//
+//  Created by Анастасия Долгих on 4/17/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
